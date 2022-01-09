9d6e601e1bcaf3374696bc80b2988f7d7925c4fe
Gedas <gedas.malakauskas@gmail.com>
2022-01-09 23:19:51 +0200

    Initial

---
+++ Console/Program.cs
@@ -0,0 +1,17 @@
+﻿var client = new MongoClient(Environment.GetEnvironmentVariable("Connection_String"));
+var database = client.GetDatabase(Environment.GetEnvironmentVariable("Database"));
+
+var collection = database.GetCollection<User>("Users");
+collection.InsertOne(new User("John", "Snow"));
+
+var user = collection.Find(user => user.FirstName == "John").FirstOrDefault();
+Console.WriteLine(user);
+
+record User(  
+    string FirstName,
+    string LastName)
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+}
