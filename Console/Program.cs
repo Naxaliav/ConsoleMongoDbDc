e37e9082e272799bfb1fbd4fa90a4aa616000faa
Gedas <gedas.malakauskas@gmail.com>
2022-01-11 18:04:29 +0200

    Rename collection to usersCollection

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,10 +1,10 @@
 ﻿var client = new MongoClient(Environment.GetEnvironmentVariable("Connection_String"));
 var database = client.GetDatabase(Environment.GetEnvironmentVariable("Database"));
 
-var collection = database.GetCollection<User>("Users");
-collection.InsertOne(new User("John", "Snow"));
+var usersCollection = database.GetCollection<User>("Users");
+usersCollection.InsertOne(new User("John", "Snow"));
 
-var user = collection.Find(user => user.FirstName == "John").FirstOrDefault();
+var user = usersCollection.Find(user => user.FirstName == "John").FirstOrDefault();
 Console.WriteLine(user);
 
 record User(  
